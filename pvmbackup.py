f63a0ccffbd99de22b8504ef7189477f10448476
Grzegorz Trochonowicz <trotyluk@users.noreply.github.com>
2023-02-05 17:24:37 +0000

    beginning 01

---
+++ pvmbackup.py
@@ -0,0 +1,4 @@
+#!python3
+# program to backup virtual machines into NAS storage
+# program use external 7z program to make backups and scp command to transport
+# to use scp the NAS ssh public key should be in the system namespace
